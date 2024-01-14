0618481de9f586d58013afc0beb4ecd6cbdbdfa5
Runjeeth Nikam <nrunjeeth@gmail.com>
2024-01-14 07:59:51 -0500

    minor fixes

--- src/models/user.ts
+++ src/models/user.ts
@@ -57,6 +57,5 @@ userSchema.statics.build = (attrs: UserAttrs) => {
 };
 
 const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
-const user = User.build({ email: "nrunjeeth@gmail.com", password: "hi" });
 
 export { User };
